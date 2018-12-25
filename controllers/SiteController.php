f93a60f3553218c2b43b2abbba44137fe785320d
jojeringnax <hollow718@gmail.com>
2018-12-25 19:51:51 +0300

    Cars

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -109,7 +109,7 @@ class SiteController extends Controller
     public function actionCarsforspot($id) {
         $cars = Car::find()->where(['spot_id' => $id])->andWhere(['not',['x_pos' => null]])->all();
         $ids = ArrayHelper::getColumn(ArrayHelper::toArray($cars), 'id');
-        Car::resetPositions($ids);
+        Car::resetPositions($ids, $id);
         $cars = Car::find()->where(['spot_id' => $id])->all();
         return json_encode(ArrayHelper::toArray($cars), JSON_UNESCAPED_UNICODE);
     }
